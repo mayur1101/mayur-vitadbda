2c51d8c75b063b041551659143c3b59e4429f64f
Mayurchaudhari1101 <mayu11011996@gmail.com>
2020-05-22 18:23:17 +0530

    Python Assignment4

--- Assignments/day1.py
+++ Assignments/day1.py
@@ -1,12 +1,13 @@
 ####1)Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
 ####between 2000 and 3200 (both included).
 ####The numbers obtained should be printed in a comma-separated sequence on a single line.
-
+##str1=""
 ##start=int(input("Enter the start number"))
 ##end=int(input("Enter the end number"))
 ##for i in range(start,end+1):
 ##    if i%7==0 and i%5!=0:
-##        print(i,end=",")
+##        str1=str1+str(i)+","
+##print(str1[:-1])
 
 
 
